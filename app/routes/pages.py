aabe6482e9d5895ce24eaf2687a5e17d057ba6c7
ShreekarSeelavantula <krishnakumar1572004@gmail.com>
2026-01-27 09:26:32 +0000

    updated the home page

--- app/routes/pages.py
+++ app/routes/pages.py
@@ -1,18 +1,37 @@
 from fastapi import APIRouter, Request
-from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 
 router = APIRouter()
 templates = Jinja2Templates(directory="app/templates")
 
-@router.get("/", response_class=HTMLResponse)
+
+@router.get("/")
 def home(request: Request):
-    return templates.TemplateResponse("home.html", {"request": request})
+    return templates.TemplateResponse(
+        "home.html",
+        {"request": request}
+    )
 
-@router.get("/signup", response_class=HTMLResponse)
+
+@router.get("/signup")
 def signup_page(request: Request):
-    return templates.TemplateResponse("signup.html", {"request": request})
+    return templates.TemplateResponse(
+        "signup.html",
+        {"request": request}
+    )
 
-@router.get("/login", response_class=HTMLResponse)
+
+@router.get("/login")
 def login_page(request: Request):
-    return templates.TemplateResponse("login.html", {"request": request})
+    return templates.TemplateResponse(
+        "login.html",
+        {"request": request}
+    )
+
+
+@router.get("/dashboard")
+def dashboard(request: Request):
+    return templates.TemplateResponse(
+        "dashboard.html",
+        {"request": request}
+    )
